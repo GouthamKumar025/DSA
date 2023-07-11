618120964dcbdc72ae8a3093c4a4efdc99aa2eb3
Goutham Kumar S <113292942+GouthamKumar025@users.noreply.github.com>
2023-07-11 09:50:47 +0530

    Update Solution.java

--- DAY 1/Solution.java
+++ DAY 1/Solution.java
@@ -1,10 +1,4 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
+/** Goutham Kumar S -- 202108017 **/
 import java.io.*;
 import java.util.*;
 
